aa6ff521bca6e23d20307bf926b56311cac046c2
Matthew Vance <MatthewVance@users.noreply.github.com>
2016-08-15 21:41:27 -0500

    Remove apt-get install and remove requirement

--- README.md
+++ README.md
@@ -1,10 +1,48 @@
 # NGINX Build Script
 
-This is an NGINX build script which compiles NGINX with a statically linked version of OpenSSL. It's been tested to work on a Raspberry Pi 2 Model B running Raspbian Jessie Lite. However, it should also work on other Debian-based Linux distros. 
+This is an NGINX build script which compiles NGINX with a statically linked version of OpenSSL. It's been tested to work on a Raspberry Pi 2 Model B running Raspbian Jessie Lite, Ubuntu 16.04, and Debian 8. However, it should also work on other Debian-based Linux distros. 
 
-This is based on the [build_nginx.sh](https://gist.github.com/MattWilcox/402e2e8aa2e1c132ee24) script from [@MattWilcox](https://github.com/MattWilcox). You can find more details about this at his [blog post](https://mattwilcox.net/web-development/setting-up-a-secure-website-with-https-and-spdy-support-under-nginx-on-a-raspberry-pi).
+The benefit of building from source is you can customize the modules to your needs and always use the latest versions of NGINX and OpenSSL. 
+
+Compiling OpenSSL will take time, especially on a Raspberry Pi, so be patient. You could remove the OpenSSL portions from the script. Doing so means NGINX will use the system version. As a result, you might not be able to use all the latest OpenSSL ciphers and features.
+
+## Usage
+
+### Installation
+
+1. `sudo mkdir /usr/local/src/nginx/`
+2. `cd /usr/local/src/nginx/`
+3. `sudo curl -L https://raw.githubusercontent.com/MatthewVance/nginx-build/master/build-nginx.sh -o build_nginx.sh`
+4. `cat build_nginx.sh` (review downloaded code before executing)
+5. `sudo chmod +x build_nginx.sh`
+6. `sudo ./build_nginx.sh`
+7. `sudo nginx`
+
+### Upgrading
+
+1. `cd /usr/local/src/nginx/`
+2. `sudo rm /usr/local/src/nginx/build_nginx.sh`
+3. `sudo curl -L https://raw.githubusercontent.com/MatthewVance/nginx-build/master/build-nginx.sh -o build_nginx.sh`
+4. `cat build_nginx.sh` (review downloaded code before executing)
+5. `sudo chmod +x build_nginx.sh`
+6. `sudo kill -QUIT $( cat /var/run/nginx.pid )`
+7. `sudo ./build_nginx.sh`
+8. `sudo nginx`
+
+## Issues
+
+If you have any problems with or questions about this image, please contact me
+through a [GitHub issue](https://github.com/MatthewVance/nginx-build/issues).
+
+## Contributing
+
+You are invited to contribute fixes and/or updates.
+
+## Acknowledgments
+
+The script was originally based on the [build_nginx.sh](https://gist.github.com/MattWilcox/402e2e8aa2e1c132ee24) script from [@MattWilcox](https://github.com/MattWilcox), but revised overtime to better fit my needs. You can find more details about the other Matt's version in his [blog post](https://mattwilcox.net/web-development/setting-up-a-secure-website-with-https-and-spdy-support-under-nginx-on-a-raspberry-pi).
 
 ## License
 
-Unless otherwise specified, all code is released under the MIT License (MIT). See the [repository's `LICENSE` file](https://github.com/MatthewVance/nginx-build/blob/master/LICENSE) for details.
+Unless otherwise specified, all code is released under the MIT License (MIT). See the [repository's `LICENSE` file](https://github.com/MatthewVance/testssl-docker/blob/master/LICENSE) for details.
 
--- build-nginx.sh
+++ build-nginx.sh
@@ -80,16 +80,34 @@ make clean
 && make install_sw
 
 # Rename the existing /etc/nginx directory so it's saved as a back-up
-mv /etc/nginx /etc/nginx-$today
+if [ -d "/etc/nginx" ]; then
+  mv /etc/nginx /etc/nginx-$today
+fi
+
+# Create NGINX cache directories if they do not already exist
+if [ ! -d "/var/cache/nginx/" ]; then
+    mkdir -p \
+    /var/cache/nginx/client_temp \
+    /var/cache/nginx/proxy_temp \
+    /var/cache/nginx/fastcgi_temp \
+    /var/cache/nginx/uwsgi_temp \
+    /var/cache/nginx/scgi_temp
+fi
+
+# Add nginx user and group if nginx user does not already exist
+id -u nginx &>/dev/null || adduser --system --no-create-home --disabled-login --disabled-password --group nginx
 
 # Build nginx, with various modules included/excluded
 cd $BPATH/$VERSION_NGINX
 mkdir -p $BPATH/nginx
-./configure --with-cc-opt="-I $STATICLIBSSL/include -I/usr/include" \
+./configure \
+--prefix=/etc/nginx \
+--with-cc-opt="-I $STATICLIBSSL/include -I/usr/include" \
 --with-ld-opt="-L $STATICLIBSSL/lib -Wl,-rpath -lssl -lcrypto -ldl -lz" \
 --with-openssl=$BPATH/$VERSION_OPENSSL \
 --with-pcre=$BPATH/$VERSION_PCRE \
 --sbin-path=/usr/sbin/nginx \
+--modules-path=/usr/lib/nginx/modules \
 --conf-path=/etc/nginx/nginx.conf \
 --error-log-path=/var/log/nginx/error.log \
 --http-log-path=/var/log/nginx/access.log \
@@ -100,6 +118,8 @@ mkdir -p $BPATH/nginx
 --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
 --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
 --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+--user=nginx \
+--group=nginx \
 --with-http_ssl_module \
 --with-http_realip_module \
 --with-http_sub_module \
@@ -121,14 +141,38 @@ mkdir -p $BPATH/nginx
 --with-http_slice_module \
 && make && make install
 
-# Rename the compiled 'default' /etc/nginx directory so its accessible as a reference to the new nginx defaults
-mv /etc/nginx /etc/nginx-default
+if [ -d "/etc/nginx-$today" ]; then
+  # Rename the compiled 'default' /etc/nginx directory so its accessible as a reference to the new nginx defaults
+  mv /etc/nginx /etc/nginx-default
 
-# Restore the previous version of /etc/nginx to /etc/nginx so the old settings are kept
-mv /etc/nginx-$today /etc/nginx
+  # Restore the previous version of /etc/nginx to /etc/nginx so the old settings are kept
+  mv /etc/nginx-$today /etc/nginx
+fi
+
+# Create NGINX systemd service file if it does not already exist
+if [ ! -e "/lib/systemd/system/nginx.service" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  FILE="/lib/systemd/system/nginx.service"
+
+  /bin/cat >$FILE <<'EOF'
+[Unit]
+Description=The NGINX HTTP and reverse proxy server
+After=syslog.target network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=forking
+PIDFile=/run/nginx.pid
+ExecStartPre=/usr/sbin/nginx -t
+ExecStart=/usr/sbin/nginx
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+fi
 
 echo "All done.";
-echo "This build has not edited your existing /etc/nginx directory.";
-echo "If things aren't working now you may need to refer to the";
-echo "configuration files the new nginx ships with as defaults,";
-echo "which are available at /etc/nginx-default";
+echo "Start with sudo systemctl start nginx"
+echo "or with sudo nginx"
