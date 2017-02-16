93230a0cbd044fbd84973181e4c9f45372f1396f
Steve M <shmick@users.noreply.github.com>
2017-02-16 07:26:01 -0500

    Update build-nginx.sh

--- build-nginx.sh
+++ build-nginx.sh
@@ -8,13 +8,13 @@ set -e -x
 # Set names of latest versions of each package
 export VERSION_PCRE=pcre-8.39
 export VERSION_ZLIB=zlib-1.2.11
-export VERSION_OPENSSL=openssl-1.1.0d
+export VERSION_OPENSSL=openssl-1.1.0e
 export VERSION_NGINX=nginx-1.11.10
 
 # Set checksums of latest versions
 export SHA256_PCRE=ccdf7e788769838f8285b3ee672ed573358202305ee361cfec7a4a4fb005bbc7
 export SHA256_ZLIB=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
-export SHA256_OPENSSL=7d5ebb9e89756545c156ff9c13cf2aa6214193b010a468a3bc789c3c28fe60df
+export SHA256_OPENSSL=57be8618979d80c910728cfc99369bf97b2a1abd8f366ab6ebdee8975ad3874c
 export SHA256_NGINX=778b3cabb07633f754cd9dee32fc8e22582bce22bfa407be76a806abd935533d
 
 # Set GPG keys used to sign downloads
