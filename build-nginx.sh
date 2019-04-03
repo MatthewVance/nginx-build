56c2ee456b5348df4e3218d0adf74b3835593113
jellemdekker <jellemdekker@users.noreply.github.com>
2019-04-03 15:43:56 +0200

    Bump PCRE to 8.43

--- build-nginx.sh
+++ build-nginx.sh
@@ -10,13 +10,13 @@ fi
 set -e -x
 
 # Set names of latest versions of each package
-version_pcre=pcre-8.42
+version_pcre=pcre-8.43
 version_zlib=zlib-1.2.11
 version_openssl=openssl-1.1.1b
 version_nginx=nginx-1.15.10
 
 # Set checksums of latest versions
-sha256_pcre=69acbc2fbdefb955d42a4c606dfde800c2885711d2979e356c0636efde9ec3b5
+sha256_pcre=0b8e7465dc5e98c757cc3650a20a7843ee4c3edf50aaf60bb33fd879690d2c73
 sha256_zlib=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
 sha256_openssl=5c557b023230413dfb0756f3137a13e6d726838ccd1430888ad15bfb2b43ea4b
 sha256_nginx=b865743abd52bce4745d0f7e7fedde3cafbaaab617b022c105e3e4e456537c3c
